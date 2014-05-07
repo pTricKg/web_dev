63d98fd5705b49d638de42a232a14f5eff68a94a
pTricKg <ptrickg1976@gmail.com>
2014-05-07 16:16:11 -0600

    more mods to home and playing with js and jquery

---
+++ javascripts/home.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+	/*$("#box1").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#box1").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#box1").click(function() {
+			$(this).effect("bounce", "slow");
+	});
+	$("#box2").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#box2").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#box2").click(function() {
+			$(this).effect("bounce", "slow");
+	});
+	$("#box3").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#box3").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#box3").click(function() {
+			$(this).effect("bounce", "slow");
+	});
+	$("#box4").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#box4").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#box4").click(function() {
+			$(this).effect("bounce", "slow");
+	});
+	$("#box5").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#box5").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#box5").click(function() {
+			$(this).effect("bounce", "slow");
+	});*/
+		
+		
+	$('div').hover(function() {
+			$(this).effect("bounce","fast");
+		});*/
+
+    $('div').mouseenter(function() {
+		$(this).fadeTo(function() {
+    	$(this).width(200);
+		});
+	});
+	$('div').mouseleave(function() {
+    	$(this).fadeTo(function() {
+			$(this).width(100);
+		});
+	}); 
+	
+});
