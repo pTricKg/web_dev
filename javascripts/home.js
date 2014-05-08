2b4c4ef3c45eec79c40c6bdc96687d34ef00c602
pTricKg <ptrickg1976@gmail.com>
2014-05-07 20:36:06 -0600

    fixed position of heading in av project page

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,45 +1,45 @@
 $(document).ready(function() {
 	$("#box1").mouseenter(function() {
-  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'fixed'});
 		});
 	$("#box1").mouseleave(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'fixed'});
 	});
 	$("#box1").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box2").mouseenter(function() {
-  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'fixed' });
 		});
 	$("#box2").mouseleave(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'fixed'});
 	});
 	$("#box2").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box3").mouseenter(function() {
-  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'fixed'});
 		});
 	$("#box3").mouseleave(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'fixed'});
 	});
 	$("#box3").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box4").mouseenter(function() {
-  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'fixed'});
 		});
-	$("#box4").mouseleave(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		$("#box4").mouseleave(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'fixed'});
 	});
 	$("#box4").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box5").mouseenter(function() {
-  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'fixed'});
 		});
 	$("#box5").mouseleave(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'fixed'});
 	});
 	$("#box5").click(function() {
 			$(this).effect("bounce", "slow");
