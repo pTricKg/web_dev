5c264b42852bc0e298372758a7d8a1704894d824
pTricKg <ptrickg1976@gmail.com>
2014-05-08 00:46:48 -0600

    screwed something up, trying to fix

--- javascripts/av_script.js
+++ javascripts/av_script.js
@@ -1,5 +1,52 @@
 $(document).ready(function() {
-    $('div').click(function() {
-    	$('div').fadeOut('slow');
+	$("#icon1").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#icon1").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#icon1").click(function() {
+			$(this).effect("clip");
+			$(this).removeAttr( "style" ).hide().fadeIn();
+	});
+	$("#icon2").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#icon2").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#icon2").click(function() {
+			$(this).effect("clip");
+			$(this).removeAttr( "style" ).hide().fadeIn();
+	});
+	$("#icon3").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#icon3").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#icon3").click(function() {
+			$(this).effect("clip");
+			$(this).removeAttr( "style" ).hide().fadeIn();
+	});
+	$("#icon4").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#icon4").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#icon4").click(function() {
+			$(this).effect("clip");
+			$(this).removeAttr( "style" ).hide().fadeIn();
+	});
+	$("#icon5").mouseenter(function() {
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+		});
+	$("#icon5").mouseleave(function() {
+  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+	});
+	$("#icon5").click(function() {
+			$(this).effect("clip");
+			$(this).removeAttr( "style" ).hide().fadeIn();
 	});
 });
