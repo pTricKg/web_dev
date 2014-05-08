aff1058126b7fdc91ecef1cd137aaaf337e221a5
pTricKg <ptrickg1976@gmail.com>
2014-05-07 18:16:36 -0600

    mod jquery on divs

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,73 +1,48 @@
 $(document).ready(function() {
-	/*$('div').mouseenter(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
-		});
-	$('div').mouseleave(function() {
-  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
-	});
-	$('div').click(function() {
-			$(this).effect("bounce", "slow");
-	});*/
 	$("#box1").mouseenter(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
 		});
 	$("#box1").mouseleave(function() {
-  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
 	});
 	$("#box1").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box2").mouseenter(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
 		});
 	$("#box2").mouseleave(function() {
-  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
 	});
 	$("#box2").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box3").mouseenter(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
 		});
 	$("#box3").mouseleave(function() {
-  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
 	});
 	$("#box3").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box4").mouseenter(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
 		});
 	$("#box4").mouseleave(function() {
-  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
 	});
 	$("#box4").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 	$("#box5").mouseenter(function() {
-  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
+  		$(this).animate({height: "125px", width: "125px", position: 'relative'});
 		});
 	$("#box5").mouseleave(function() {
-  		$(this).animate({height: "57px", width: "57px", position: 'relative'});
+  		$(this).animate({height: "100px", width: "100px", position: 'relative'});
 	});
 	$("#box5").click(function() {
 			$(this).effect("bounce", "slow");
 	});
 		
-		
-	/*$('div').hover(function() {
-			$(this).effect("bounce","fast");
-		});*/
-/*
-    $('div').mouseenter(function() {
-		$(this).fadeTo(function() {
-    	$(this).width(200);
-		});
-	});
-	$('div').mouseleave(function() {
-    	$(this).fadeTo(function() {
-			$(this).width(100);
-		});
-	}); */
-	
 });
