1f8cace2074edee4f5f0ab846f2686c6b1e48168
pTricKg <ptrickg1976@gmail.com>
2014-05-31 23:01:06 -0600

    Added click

--- js/animate_img.js
+++ js/animate_img.js
@@ -2,16 +2,20 @@ $(document).ready(function() {
     $(document).keydown(function(key) {
         switch(parseInt(key.which,10)) {
 			case 65: // a
-				$('img').animate({left: "-=10px"}, 'fast');// move with key a
+				$('img').click(
+				$('img').animate({left: "-=10px"}, 'fast'));// move with key a
 				break;
 			case 83: // s
-				$('img').animate({top: "+=10px"}, 'fast');// moves with key
+				$('img').click(
+				$('img').animate({top: "+=10px"}, 'fast'));// moves with key
 				break;
 			case 87: // w
-				$('img').animate({top: "-=10px"}, 'fast');
+				$('img').click(
+				$('img').animate({top: "-=10px"}, 'fast'));
 				break;
 			case 68:  // d
-				$('img').animate({left: "+=10px"}, 'fast');
+				$('img').click(
+				$('img').animate({left: "+=10px"}, 'fast'));
 				break;
 			default:
 				break;
