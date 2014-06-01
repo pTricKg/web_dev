71b1282913afa40907b07604c6c837ece035fd2d
pTricKg <ptrickg1976@gmail.com>
2014-05-31 23:04:11 -0600

    Removed click

--- js/animate_img.js
+++ js/animate_img.js
@@ -2,20 +2,16 @@ $(document).ready(function() {
     $(document).keydown(function(key) {
         switch(parseInt(key.which,10)) {
 			case 65: // a
-				$('img').click(
-				$('img').animate({left: "-=10px"}, 'fast'));// move with key a
+				$('img').animate({left: "-=10px"}, 'fast');// move with key a
 				break;
 			case 83: // s
-				$('img').click(
-				$('img').animate({top: "+=10px"}, 'fast'));// moves with key
+				$('img').animate({top: "+=10px"}, 'fast');// moves with key
 				break;
 			case 87: // w
-				$('img').click(
-				$('img').animate({top: "-=10px"}, 'fast'));
+				$('img').animate({top: "-=10px"}, 'fast');
 				break;
 			case 68:  // d
-				$('img').click(
-				$('img').animate({left: "+=10px"}, 'fast'));
+				$('img').animate({left: "+=10px"}, 'fast');
 				break;
 			default:
 				break;
