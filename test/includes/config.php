c6ecaf8f14c8e12ca5c1a4ed986d3f5896660863
pTricKg <ptrickg1976@gmail.com>
2015-06-07 17:02:55 -0600

    add blog to home

---
+++ test/includes/config.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('IN_BLOG'))
+{
+	exit;
+}
+
+$sqlconfig = array();
+$sqlconfig['username'] = 'mine_blog_user';			 // database username
+$sqlconfig['password'] = 'ThisIsMyOwn9976';			 // database password
+$sqlconfig['host']     = 'ptrickgcom.fatcowmysql.com';	 // database host (usually localhost)
+$sqlconfig['dbname']   = 'mine_blog';   			 // database name
+
+?>
