29eb933b45b4692ead36eff265af5cfdc5500f8b
pTricKg <ptrickg1976@gmail.com>
2016-07-01 16:24:00 -0600

    date update
    date update

--- test/index.php
+++ test/index.php
@@ -262,7 +262,7 @@ include('includes/miniblog.php');
 </p>
 
 <p align="center" class="text">
-	<span class="footer">Last Revised 2016.06 by P.G.</span>
+	<span class="footer">Last Revised 2016.07 by P.G.</span>
 </p>
 			</div>
 		</th>
